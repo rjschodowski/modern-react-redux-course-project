e21a190e2218ee474bbde1d573f3c1b81d50ad6f
R.J <rjschodowski@hotmail.com>
2021-07-12 11:52:01 -0400

    props added to approval card component

--- src/index.js
+++ src/index.js
@@ -11,25 +11,37 @@ if (module.hot) {
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        content="Nice blog post"
-        avatar={faker.image.image()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00AM"
-        content="I like the subject"
-        avatar={faker.image.image()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 5:00PM"
-        content="I like the writing"
-        avatar={faker.image.image()}
-      />
+
+      <ApprovalCard>
+        <h4>Warningish</h4>
+        Are you sure about this man?
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog post"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          content="I like the subject"
+          avatar={faker.image.image()}
+        />
+        </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          content="I like the writing"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
